fix(distance): optimize computation using np function

knn.py
@@ -3,12 +3,7 @@ from scipy import stats
 
 
 def distance_matrix(a, b):
-    # calculer la distance sur chaque pixel et chaque couleur
-    diff = np.sqrt(a*a + b*b - 2*a*b)
-    # faire la somme matriciel des couelurs RGB sur chaque pixel et chaque image
-    somm = np.sum(diff, (1))
-    # prendre la racine de la somme
-    dists = np.sqrt(somm)
+    dists = np.linalg.norm(a - b, axis=1)
     return dists
 
 

tests/test_knn.py
@@ -16,12 +16,12 @@ c1 = np.array([a, a, b])
 def test_distance_matrix():
     dists = distance_matrix(a1, b1)
     assert np.shape(dists) == (3, )
-    assert np.array_equal(dists, [3., 3., 3.])
+    assert np.array_equal(dists, [np.sqrt(27), ]*3)
 
     dists1 = distance_matrix(a1, c1)
     assert np.shape(dists1) == (3, )
     assert dists1[0] == 0.
-    assert dists[1] == dists[2] == 3.
+    assert dists[1] == dists[2] == np.sqrt(27)
 
     return
 