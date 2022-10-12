6fc6dfd2eca8f3cb3821210336a92368cb70c891
Alexandre Colas <alexandr.colas@free.fr>
2022-10-12 16:12:29 +0200

    feat(knn): compute and plot results

---
+++ main.py
@@ -0,0 +1,43 @@
+from matplotlib import pyplot as plt
+from knn import evaluate_knn
+from read_cifar import read_cifar, read_cifar_batch, split_dataset
+
+
+split_test = False
+
+while split_test != True:
+    str_split_factor = input(
+        'Enter a float between 0 and 1 which determines the split factor between training and test sets :  ')
+
+    try:
+        split_factor = float(str_split_factor)
+        if (type(split_factor) == float) and (0. <= split_factor < 1.):
+            split_test = True
+
+        else:
+            print('You must enter a float between 0 and 1 :',
+                  split_factor, 'is invalid')
+            split_test = False
+    except Exception as e:
+        split_test = False
+        print(e)
+
+
+dict = read_cifar_batch('data/data_batch_1')
+(data, labels) = dict
+
+data_train, labels_train, data_test, labels_test = split_dataset(
+    data, labels, split_factor)
+
+accuracies = []
+list_k = list(range(1, 21, 1))
+print(list_k)
+for k in list_k:
+    print(k)
+    k_accuracy = evaluate_knn(data_train, labels_train,
+                              data_test, labels_test, k)
+    accuracies.append(k_accuracy)
+
+print(accuracies)
+plt.plot(list_k, accuracies, 'bo')
+plt.show()
