a6c935c55141f5d2ea5a544023867c831fe6dae3
Alexandre Colas <alexandr.colas@free.fr>
2022-10-21 21:42:37 +0200

    feat(method): allow choice between optimized or not knn method

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from modules.helper import choose_classification_method, choose_split_factor, pl
 
 chosen_method = choose_classification_method()
 split_factor = choose_split_factor()
-
+k_max = 20
 
 dict = read_cifar('data')
 (data, labels) = dict
@@ -12,19 +12,22 @@ dict = read_cifar('data')
 data_train, labels_train, data_test, labels_test = split_dataset(
     data, labels, split_factor)
 
-# accuracies = []
-# list_k = list(range(1, 21, 1))
-# print(list_k)
-# for k in list_k:
-#     print(k)
-#     k_accuracy = evaluate_knn(data_train, labels_train,
-#                               data_test, labels_test, k)
-#     accuracies.append(k_accuracy)
 
-# print(accuracies)
+if (chosen_method == 0):
+    accuracies = []
+    list_k = list(range(1, 21, 1))
+    for k in list_k:
+        print('Evaluating KNN for k = ', k)
+        k_accuracy = evaluate_knn(data_train, labels_train,
+                                  data_test, labels_test, k)
+        accuracies.append(k_accuracy)
 
-k_max = 20
-accuracy_for_all_k = evaluate_knn_optimized(data_train, labels_train,
-                                            data_test, labels_test, k_max)
+    plot_and_save_fig(k_max, accuracies, split_factor)
 
-plot_and_save_fig(k_max, accuracy_for_all_k, split_factor)
+
+elif (chosen_method == 1):
+
+    accuracy_for_all_k = evaluate_knn_optimized(data_train, labels_train,
+                                                data_test, labels_test, k_max)
+
+    plot_and_save_fig(k_max, accuracy_for_all_k, split_factor)
