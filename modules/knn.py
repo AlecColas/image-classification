fffaec9bb91defaeb4a2a057fade637b7e02ef4b
Alexandre Colas <alexandr.colas@free.fr>
2022-10-21 17:23:33 +0200

    wip(optimized): apply computation with matrix on optimized version of knn

--- modules/knn.py
+++ modules/knn.py
@@ -51,42 +51,6 @@ def evaluate_knn(data_train, labels_train, data_test, labels_test, k: int):
     return compute_accuracy(labels_test, computed_labels_for_test_images)
 
 
-def evaluate_classification_for_range_kmax(data_train, labels_train, data_test, k_max: int):
-
-    # First we pick a test image in data_test
-    # We get the labels of the k-nn for k = k_max
-    # Thus we have all k-nn for k in range (1, k_max)
-    # Then we compute the mode (nearest class' label) for all k in range (1,k_max) for this sample image
-
-    # We reproduce this to all rows in data_test (each row representing 1 image)
-    # Then we extract the computed labels, for each k in range (1, k_max+1), for each test image
-
-    L_data_train = np.shape(data_train)[0]
-    L_data_test = len(data_test)
-
-    computed_labels_for_all_k = []
-
-    for i in range(L_data_test):
-        print('Computing distance matrix for sample ',
-              str(i), 'over', str(L_data_test))
-
-        matrix_test_i = np.array([data_test[i], ] * L_data_train)
-        dists = distance_matrix(data_train, matrix_test_i)
-
-        labels_of_knn = knn_predict(dists, labels_train, k_max)
-
-        computed_classifications_for_test_i_and_all_k = []
-
-        for j in range(1, k_max+1):
-            computed_classifications_for_test_i_and_all_k.append(
-                classify_with_mode(labels_of_knn[:j]))
-
-        computed_labels_for_all_k.append(
-            computed_classifications_for_test_i_and_all_k)
-
-    return np.array(computed_labels_for_all_k)
-
-
 def compute_accuracy_for_range_k(labels_test, computed_labels_for_all_k, k_max):
     nb_labels = len(labels_test)
 
@@ -115,9 +79,9 @@ def evaluate_knn_optimized(data_train, labels_train, data_test, labels_test, k_m
     if (k_max <= 0 or k_max > L_data_train):
         return
 
+    dists = distance_matrix(data_train, data_test)
     # get the computed labels for data_test, for all k in range (1,k_max)
-    computed_labels_for_all_k = evaluate_classification_for_range_kmax(
-        data_train, labels_train, data_test, k_max)
+    computed_labels_for_all_k = knn_predict(dists, labels_train, k_max)
 
     # compute the accuracy of the knn method for all k in range (1, k_max)
     accuracy_for_all_k = compute_accuracy_for_range_k(
